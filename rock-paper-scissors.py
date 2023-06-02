9acb50df275efb5bd87efc4d702a8b77b1bc3f38
huntingwall8910 <130617226+huntingwall8910@users.noreply.github.com>
2023-06-01 21:26:32 -0400

    Build rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,1 +1,23 @@
- 
+import random
+import sys
+user_choice = input("Rock, Paper, Scissors: ")
+choices = {
+    'Rock':{'beats':'Scissors'},
+    'Paper':{'beats':'Rock'},
+    'Scissors':{'beats':'Paper'}
+}
+computer_choice=random.choice(list(choices.keys()))
+if user_choice not in choices:
+    print("Invalid Input")
+    sys.exit()
+    # if not exit result still print
+else:
+    pass
+    #underrated function
+if user_choice == computer_choice:
+    print("Tie")
+elif choices[user_choice]['beats'] == computer_choice:
+    #key comparison
+    print("Win")
+else:
+    print("Lost")
